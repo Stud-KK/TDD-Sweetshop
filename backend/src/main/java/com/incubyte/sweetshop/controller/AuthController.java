eaacfb0266fab091128ad4e121677ed77326f977
Komal Kalshetti <komalkalshetti989@gmail.com>
2025-12-13 19:16:03 +0530

    feat(auth): delegate register request to auth service(green)

--- backend/src/main/java/com/incubyte/sweetshop/controller/AuthController.java
+++ backend/src/main/java/com/incubyte/sweetshop/controller/AuthController.java
@@ -1,21 +1,24 @@
 package com.incubyte.sweetshop.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 import com.incubyte.sweetshop.controller.dto.RegisterRequest;
+import com.incubyte.sweetshop.service.AuthService;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
-
-        public void register(@Valid @RequestBody RegisterRequest request) {
+    public void register(@Valid @RequestBody RegisterRequest request) {
+        authService.register(request);
     }
 }
